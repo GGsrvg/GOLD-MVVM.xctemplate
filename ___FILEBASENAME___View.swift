b0a48f70c28d18b42dc70c4eeb41199e7d5ae645
GGsrvg <x@y.z>
2021-02-04 21:39:50 +0300

    init

---
+++ ___FILEBASENAME___View.swift
@@ -0,0 +1,22 @@
+//___FILEHEADER___
+
+import UIKit
+
+class ___FILEBASENAMEASIDENTIFIER___: UIView {
+    
+    init() {
+        super.init(frame: .zero)
+        self.setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupView() {
+        self.setupConstraints()
+    }
+    
+    private func setupConstraints() {
+    }
+}
---
+++ ___FILEBASENAME___ViewController.swift
@@ -0,0 +1,10 @@
+//___FILEHEADER___
+
+import UIKit
+
+class ___FILEBASENAMEASIDENTIFIER___: BaseViewController<___VARIABLE_productName:identifier___View, ___VARIABLE_productName:identifier___ViewModel, ___VARIABLE_productName:identifier___InitVC> {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
